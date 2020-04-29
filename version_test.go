d4aa9ccfbb7d8fcbd9e4ecdcaf80719b2cd8aec8
knqyf263 <knqyf263@gmail.com>
2020-04-29 17:10:49 +0300

    first commit

---
+++ version_test.go
@@ -0,0 +1,80 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewVersion(t *testing.T) {
+	tests := []struct {
+		name    string
+		ver     string
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			ver:  "1.2.3",
+		},
+		{
+			name: "happy path with pre suffix",
+			ver:  "1.2.3_alpha1",
+		},
+		{
+			name: "happy path with post suffix",
+			ver:  "1.2.3-r1",
+		},
+		{
+			name: "happy path with 2 pre suffixes",
+			ver:  "0.1.0_alpha_pre2",
+		},
+		{
+			name: "happy path with alphabets",
+			ver:  "1.0b",
+		},
+		{
+			name:    "sad path",
+			ver:     "1.0bc",
+			wantErr: true,
+		},
+		{
+			name:    "invalid symbol",
+			ver:     "1.0!",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewVersion(tt.ver)
+			if (err != nil) != tt.wantErr {
+				assert.Equal(t, tt.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestVersion_Compare(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(tt.v1+" vs "+tt.v2, func(t *testing.T) {
+			a := newVersion(tt.v1)
+			b := newVersion(tt.v2)
+
+			got := a.Compare(b)
+
+			var expected int
+			switch tt.expected {
+			case ">":
+				expected = apkVersionGreater
+			case "<":
+				expected = apkVersionLess
+			case "=":
+				expected = apkVersionEqual
+			default:
+				require.Fail(t, "unknown symbol: %s", tt.expected)
+			}
+
+			assert.Equal(t, expected, got)
+		})
+	}
+}
